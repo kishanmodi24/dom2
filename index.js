be1a9de5630ed2fd092e714d51055260c4b9fd45
kishanmodi24 <connectkishanmodi@gmail.com>
2024-04-07 20:16:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var con = document.querySelector("#container")
+var lov = document.querySelector("i")
+
+con.addEventListener("click",function(){
+    lov.style.transform = "translate(-50%,-50%)"
+})
